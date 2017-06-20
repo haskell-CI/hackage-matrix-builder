9abfe9860bb8615b66efa1b7d6515c56861e208d
Andika D Riyandi <andika.riyan@gmail.com>
2017-06-20 13:40:01 +0700

    Add router and main query

--- purescript-halogen/src/Main.purs
+++ purescript-halogen/src/Main.purs
@@ -1,12 +1,115 @@
 module Main where
 
 import Prelude
+import Control.Coroutine as CR
+import Control.Coroutine.Aff as CRA
+import Control.Monad.Aff (Aff)
+import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Except (runExcept)
+import Control.Monad.Aff.Console (log)
+import Control.Monad.Eff.Console (CONSOLE)
+import Data.Either (Either(..))
+import Data.Either.Nested (Either2)
+import Data.Functor.Coproduct.Nested (Coproduct2)
+import Data.Foreign (toForeign)
+import Data.Maybe (Maybe(..))
+import Data.String as Str
+import DOM (DOM)
+import DOM.Event.EventTarget (eventListener, addEventListener) as DOM
+import DOM.HTML (window) as DOM
+import DOM.HTML.Event.EventTypes as ET
+import DOM.HTML.Event.HashChangeEvent as HCE
+import DOM.HTML.Event.Types (HashChangeEvent, readHashChangeEvent) as DOM
+import DOM.HTML.Types (windowToEventTarget) as DOM
+import Halogen as H
+import Halogen.Component.ChildPath as CP
+import Halogen.HTML as HH
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 import Container as Container
+import Router as Router
+import CSS.Display (Display, block, displayNone)
+import Halogen.HTML.CSS
 
+type State =
+  { display :: Display }
+
+initialState :: State
+initialState =
+  { display: displayNone }
+
+data Query a
+  = Initialize a
+  | Finalize a
+  | Response a
+
+type ChildQuery = Coproduct2 Router.Query
+                             Container.Query
+
+type ChildSlot = Either2 Unit Unit
+
+type UIEff eff = Aff (console :: CONSOLE | eff)
+
+ui :: forall eff. H.Component HH.HTML Query Unit Void (UIEff eff)
+ui = H.lifecycleParentComponent
+  { initialState: const initialState
+  , render
+  , eval
+  , initializer: Just (H.action Initialize)
+  , finalizer: Just (H.action Finalize)
+  , receiver: const Nothing
+  }
+  where
+    render :: State -> H.ParentHTML Query ChildQuery ChildSlot (UIEff eff)
+    render state =
+      HH.div_
+        [ HH.slot' CP.cp1 unit Router.ui unit absurd
+	, HH.slot' CP.cp2 unit Container.ui unit absurd
+	] 
+
+    eval :: Query ~> H.ParentDSL State Query ChildQuery ChildSlot Void (UIEff eff)
+    eval (Initialize next) = do
+      H.liftAff $ log "Initialize Root"
+      pure next
+    eval (Finalize next) = do
+      pure next
+    eval (Response next) = do
+      pure next
+
+-- A producer coroutine that emits messages whenever the window emits a
+-- `hashchange` event.
+hashChangeProducer
+  :: forall eff
+   . CR.Producer DOM.HashChangeEvent (Aff (avar :: AVAR, dom :: DOM | eff)) Unit
+hashChangeProducer = CRA.produce \emit ->
+  let
+    emitter e =
+      case runExcept (DOM.readHashChangeEvent (toForeign e)) of
+        Left _ -> pure unit
+        Right hce -> emit (Left hce)
+  in
+    liftEff $
+      DOM.window
+        >>= DOM.windowToEventTarget
+        >>> DOM.addEventListener ET.hashchange (DOM.eventListener emitter) false
+
+-- A consumer coroutine that takes the `query` function from our component IO
+-- record and sends `ChangeRoute` queries in when it receives inputs from the
+-- producer.
+hashChangeConsumer
+  :: forall eff
+   . (Query ~> Aff (HA.HalogenEffects eff))
+  -> CR.Consumer DOM.HashChangeEvent (Aff (HA.HalogenEffects eff)) Unit
+hashChangeConsumer query = CR.consumer \event -> do
+  let hash = Str.drop 1 $ Str.dropWhile (_ /= '#') $ HCE.newURL event
+  query $ H.action $ Router.ChangeRoute hash
+  pure Nothing
+  
 main :: forall eff. Eff (HA.HalogenEffects eff) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI Container.ui unit body
+  io <- runUI ui unit body
+  CR.runProcess (hashChangeProducer CR.$$ hashChangeConsumer io.query)
+  
--- purescript-halogen/src/Router.purs
+++ purescript-halogen/src/Router.purs
@@ -1,1 +1,70 @@
 module Router where
+
+import Prelude
+import Data.Array as A
+import Data.Maybe (Maybe(..))
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+
+type State = { history :: Array String }
+
+data Query a = ChangeRoute String a
+
+ui :: forall m. H.Component HH.HTML Query Unit Void m
+ui =
+  H.component
+    { initialState: const initialState
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+    initialState :: State
+    initialState = { history: [] }
+
+    render :: State -> H.ComponentHTML Query
+    render state =
+      HH.nav
+        [ HP.id_ "menu"
+	, HP.class_ (H.ClassName "clearfix")
+	]
+	[ HH.div
+	    [ HP.classes (H.ClassName <$> ["item","left","logo-container","clearfix"]) ]
+	    [ HH.img
+	        [ HP.class_ (H.ClassName "logo")
+		, HP.src "//www.haskell.org/static/img/logo.png"
+		, HP.alt "Haskell Logo"
+                ]
+            , HH.h1
+                [ HP.class_ (H.ClassName "logo-text") ]
+                [ HH.text "Hackage Matrix Builder"
+                , HH.i_ [ HH.text "3rd"]
+                ]
+	    ]
+        , HH.div
+	    [ HP.classes (H.ClassName <$> ["item","link","left"]) ]
+	    [ HH.a [ HP.href "#" ] [ HH.text "Home" ] ]
+        , HH.div
+	    [ HP.classes (H.ClassName <$> ["item","link","left"]) ]
+	    [ HH.a [ HP.href "#latest" ] [ HH.text "Latest builds" ] ]
+        , HH.div
+	    [ HP.classes (H.ClassName <$> ["item","link","left"]) ]
+	    [ HH.a [ HP.href "#packages" ] [ HH.text "Packages" ] ]
+        , HH.div
+	    [ HP.classes (H.ClassName <$> ["item","search","right","clearfix"]) ]
+	    [ HH.div
+	        [ HP.class_ (H.ClassName "text") ]
+		[ HH.text "Package Search" ]
+	    , HH.input
+		[ HP.type_ HP.InputText
+	        , HP.class_ (H.ClassName "input")
+		, HP.id_ "search"
+		]
+            ]
+        ]
+    eval :: Query ~> H.ComponentDSL State Query Void m
+    eval = case _ of
+      ChangeRoute msg next -> do
+        -- H.modify \st -> { history: st.history `A.snoc` msg }
+        pure next
