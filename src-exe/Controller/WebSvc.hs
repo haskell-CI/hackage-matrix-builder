haskell-CI/hackage-matrix-builder

commit b03e4c5903766956822d691f3c4c04e94a322d34

Implement /api/v2/idxstates API
The `GET /api/v2/idxstates/latest` endpoint is particularly useful for
the v2 queue API for which the idxstate parameter is mandatory when
creating a new queue entry.

src-exe/Controller/WebSvc.hs
@@ -145,6 +145,9 @@ server = tagListH
     :<|> usrListH
 
     -- v2
+    :<|> idxStatesH
+    :<|> idxStatesLatestH
+
     :<|> packagesH
     :<|> packagesTagsH
     :<|> reportsH
@@ -293,7 +296,21 @@ server = tagListH
         withDbcGuard (pkgnExists pname) $ \dbconn -> queryCellReport dbconn (PkgId pname pver) gv ptime
 
     ----------------------------------------------------------------------------
-    -- package v2 --------------------------------------------------------------
+    -- v2/idxstates-------------------------------------------------------------
+
+    idxStatesH :: Maybe PkgIdxTs -> Maybe PkgIdxTs -> AppHandler [PkgIdxTs]
+    idxStatesH Nothing Nothing     = withDbc $ \dbconn -> (coerce :: [Only PkgIdxTs] -> [PkgIdxTs]) <$> PGS.query_ dbconn "SELECT ptime FROM idxstate ORDER by ptime"
+    idxStatesH (Just lb) Nothing   = withDbc $ \dbconn -> (coerce :: [Only PkgIdxTs] -> [PkgIdxTs]) <$> PGS.query dbconn "SELECT ptime FROM idxstate WHERE ptime >= ? ORDER by ptime" (Only lb)
+    idxStatesH Nothing (Just ub)   = withDbc $ \dbconn -> (coerce :: [Only PkgIdxTs] -> [PkgIdxTs]) <$> PGS.query dbconn "SELECT ptime FROM idxstate WHERE ptime <= ? ORDER by ptime" (Only ub)
+    idxStatesH (Just lb) (Just ub) = withDbc $ \dbconn -> (coerce :: [Only PkgIdxTs] -> [PkgIdxTs]) <$> PGS.query dbconn "SELECT ptime FROM idxstate WHERE ptime BETWEEN ? AND ? ORDER by ptime" (lb,ub)
+
+    idxStatesLatestH :: AppHandler PkgIdxTs
+    idxStatesLatestH = do
+        [Only is] <- withDbc $ \dbconn -> PGS.query_ dbconn "SELECT max(ptime) FROM idxstates"
+        pure is
+
+    ----------------------------------------------------------------------------
+    -- v2/packages -------------------------------------------------------------
 
     packagesH :: AppHandler [PkgN]
     packagesH = withDbc $ \dbconn -> (coerce :: [Only PkgN] -> [PkgN]) <$> PGS.query_ dbconn "SELECT pname FROM pkgname ORDER by pname"

src-exe/Controller/Api.hs
@@ -58,6 +58,9 @@ type ControllerApi m =
   :<|> "v1.0.0" :> "user"                :> "name" :> Capture "username" UserName                      :> Get '[JSON] UserPkgs
 
   -- New-style API; we stick w/ the more common RESTful convention of using plural nouns for listable collections
+  :<|> "v2" :> "idxstates" :> QueryParam "min" PkgIdxTs :> QueryParam "max" PkgIdxTs :> Get '[JSON] [PkgIdxTs]
+  :<|> "v2" :> "idxstates" :> "latest" :> Get '[JSON] PkgIdxTs
+
   :<|> "v2" :> "packages" :> Get '[JSON] [PkgN]
   :<|> "v2" :> "packages" :> Capture "pkgname" PkgN :> "tags" :> Get '[JSON] (Set TagName)
   :<|> "v2" :> "packages" :> Capture "pkgname" PkgN :> "reports" :> Get '[JSON] (Set PkgIdxTs)

commit 50c678e3dec29ae59fd7d2b1d23e9c4b92b2a65a

Fix typo introduced in b03e4c59037669568

src-exe/Controller/WebSvc.hs
@@ -307,7 +307,7 @@ server = tagListH
 
     idxStatesLatestH :: AppHandler PkgIdxTs
     idxStatesLatestH = do
-        [Only is] <- withDbc $ \dbconn -> PGS.query_ dbconn "SELECT max(ptime) FROM idxstates"
+        [Only is] <- withDbc $ \dbconn -> PGS.query_ dbconn "SELECT max(ptime) FROM idxstate"
         pure is
 
     ----------------------------------------------------------------------------