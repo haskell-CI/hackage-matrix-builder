89a3a0fc20f6ab34c4e960b1c7f2370bf02057ab
Herbert Valerio Riedel <hvr@gnu.org>
2017-09-28 22:36:59 +0200

    Define new data-structure representing unions of PkgIdxTs ranges

---
+++ src-lib/PkgIdxRanges.hs
@@ -0,0 +1,144 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+
+module PkgIdxRanges
+    ( IdxRanges
+    , fromList
+    , toList
+    , member
+    , null
+    , isBounded
+    , intersection
+    , intersectionN
+    ) where
+
+import qualified Prelude
+import           Prelude.Local                        hiding (normalise, null,
+                                                       toList)
+
+import           PkgId
+
+import qualified Data.Set                             as Set
+import           Database.PostgreSQL.Simple.FromField
+import           Database.PostgreSQL.Simple.Range
+import           Database.PostgreSQL.Simple.ToField
+import qualified Database.PostgreSQL.Simple.Types     as PGS
+
+-- | Represents union of 'PkgIdxTs'-intervals
+--
+-- The upper bound may be unbounded.
+newtype IdxRanges = IRA [(PkgIdxTs,Maybe PkgIdxTs)]
+                  deriving (NFData,Eq)
+
+instance Show IdxRanges where
+    show (IRA rs) = "(" ++ intercalate " + " (map go rs) ++ ")"
+        where
+          go (PkgIdxTs l,Nothing)           = "["++show l++",+inf)"
+          go (PkgIdxTs l,Just (PkgIdxTs r)) = "["++show l++","++show r++")"
+
+-- | Serialises to @int4range[]@
+instance ToField IdxRanges where
+    toField (IRA rs) = Many [toField (PGS.PGArray rs'), Plain "::int4range[]" ]
+      where
+        rs' :: [PGRange Int]
+        rs' = [ PGRange (Inclusive (unPkgIdxTs l)) (maybe PosInfinity (Exclusive . unPkgIdxTs) r) | (l,r) <- rs ]
+
+-- | Deserialises from @int4range[]@
+instance FromField IdxRanges where
+    fromField f dat = go <$> fromField f dat
+      where
+        go :: PGS.PGArray (PGRange Int) -> IdxRanges
+        go (PGS.PGArray rs) = IRA (map go2 rs)
+
+        go2 :: PGRange Int -> (PkgIdxTs,Maybe PkgIdxTs)
+        go2 (PGRange (Inclusive l) (Exclusive r)) = (PkgIdxTs l,Just $ PkgIdxTs r)
+        go2 (PGRange (Inclusive l) PosInfinity)   = (PkgIdxTs l,Nothing)
+        go2 (PGRange (Exclusive l) r) = go2 (PGRange (Inclusive (l+1)) r)
+        go2 (PGRange l (Inclusive r)) = go2 (PGRange l (Exclusive (r+1)))
+        go2 (PGRange PosInfinity NegInfinity) = (PkgIdxTs 0, Just (PkgIdxTs 0))
+        go2 _ = error "FromField[IdxRanges]"
+
+----------------------------------------------------------------------------
+-- (de)construction
+
+fromList :: [(PkgIdxTs,Maybe PkgIdxTs)] -> IdxRanges
+fromList = normalise . IRA
+
+toList :: IdxRanges -> [(PkgIdxTs,Maybe PkgIdxTs)]
+toList (IRA rs) = rs
+
+----------------------------------------------------------------------------
+-- predicates
+
+member :: PkgIdxTs -> IdxRanges -> Bool
+member t (IRA rs) = any f rs
+  where
+    f (t0,Nothing) = t0 <= t
+    f (t0,Just t1) = t0 <= t && t < t1
+
+-- | Test whether 'IdxRanges' is the empty set
+null :: IdxRanges -> Bool
+null (IRA rs) = Prelude.null rs
+
+-- | Test whether 'IdxRanges' represents an interval with non-infinite bounds.
+isBounded :: IdxRanges -> Bool
+isBounded (IRA []) = True
+isBounded (IRA rs) = (isJust . snd . last) rs
+
+----------------------------------------------------------------------------
+-- operations
+
+-- union monoid
+instance Monoid IdxRanges where
+    mempty = IRA []
+
+    mappend (IRA []) y      = y
+    mappend x (IRA [])      = x
+    mappend (IRA x) (IRA y) = normalise (IRA (x++y))
+
+intersection :: IdxRanges -> IdxRanges -> IdxRanges
+intersection (IRA []) _ = mempty
+intersection _ (IRA []) = mempty
+intersection a0 b0 = normalise (IRA $ Set.toList res')
+  where
+    res' = Set.fromList (concatMap splitup1 as) `Set.intersection` Set.fromList (concatMap splitup1 bs)
+
+    IRA as = normalise a0
+    IRA bs = normalise b0
+
+    splitps :: Set PkgIdxTs
+    splitps = Set.fromList (concatMap f as) <> Set.fromList (concatMap f bs)
+
+    f (x,Just y)  = [x,y]
+    f (x,Nothing) = [x]
+
+    splitup1 (l,mr) = zipWith (,) (l : lxs) (map Just lxs ++ [mr])
+      where
+        tmp = snd $ fmap Set.toList $ Set.split l splitps
+        lxs = case mr of
+                Nothing -> tmp
+                Just r  -> takeWhile (<r) tmp
+
+intersectionN :: NonEmpty IdxRanges -> IdxRanges
+intersectionN = foldl1 intersection
+
+----------------------------------------------------------------------------
+-- internal helper
+
+normalise :: IdxRanges -> IdxRanges
+normalise (IRA []) = IRA []
+normalise (IRA rs) = IRA (go (sort (filter ne rs)))
+  where
+    go []               = []
+    go ((l1,Nothing):_) = [(l1,Nothing)]
+    go [x@(_,Just _)]   = [x]
+    go (lr1@(l1,Just r1):lr2@(l2,mr2):rest)
+        | r1 < l2    = lr1 : go (lr2:rest) -- don't merge
+        | otherwise  = case mr2 of -- merge
+                         Nothing -> go ((l1,Nothing):rest)
+                         Just r2 -> go ((l1,Just (max r1 r2)):rest)
+
+
+    ne :: (PkgIdxTs,Maybe PkgIdxTs) -> Bool
+    ne (ts1,Just ts2) = ts1 < ts2
+    ne (_,Nothing)    = True
--- src-test/Test.hs
+++ src-test/Test.hs
@@ -10,8 +10,10 @@ import           Test.Tasty.QuickCheck as QC
 
 import           PkgId
 import qualified PkgIdxTsSet           as PIS
+import qualified PkgIdxRanges          as IR
 
 import qualified Data.Set              as Set
+import           Data.Word
 
 main :: IO ()
 main = defaultMain tests
@@ -20,9 +22,10 @@ tests :: TestTree
 tests = testGroup "Tests" [properties]
 
 properties :: TestTree
-properties = testGroup "Properties" [qcProps]
+properties = testGroup "Properties" [qcProps2,qcProps1]
 
-qcProps = testGroup "(checked by QuickCheck)"
+
+qcProps1 = testGroup "PkgIdxTsSet"
   [ QC.testProperty "Eq" $
       \x -> (x :: Int) == x
 
@@ -85,5 +88,44 @@ qcProps = testGroup "(checked by QuickCheck)"
 
 wrapIntoRange :: (Int,Int) -> Int -> Int
 wrapIntoRange (l,u) x = l + ((x-l) `mod` (u-l))
+
+----------------------------------------------------------------------------
+
+qcProps2 = testGroup "PkgIdxRanges"
+  [ QC.testProperty "eq1" $
+      \rs -> IR.fromList rs == IR.fromList (reverse rs)
+
+  , QC.testProperty "eq2" $
+      \rs -> irFromList8 rs == irFromList8 (reverse rs)
+
+  , QC.testProperty "eq3" $
+      \rs1 rs2 -> let rs1' = irFromList8 rs1
+                      rs2' = irFromList8 rs2
+                  in (irToSet rs1' == irToSet rs2') == (rs1' == rs2')
+
+  , QC.testProperty "union" $
+      \rs1 rs2 -> let rs1' = irFromList8 rs1
+                      rs2' = irFromList8 rs2
+                      rs12' = mappend rs1' rs2'
+                  in (irToSet rs1' `mappend` irToSet rs2') == irToSet rs12'
+
+  , QC.testProperty "intersection" $
+      \rs1 rs2 -> let rs1' = irFromList8 rs1
+                      rs2' = irFromList8 rs2
+                      rs12' = IR.intersection rs1' rs2'
+                  in (irToSet rs1' `Set.intersection` irToSet rs2') == irToSet rs12'
+
+  ]
+
+irFromList8 :: [(Word8, Word8)] -> IR.IdxRanges
+irFromList8 = IR.fromList . map f
+  where
+    f :: (Word8,Word8) -> (PkgIdxTs,Maybe PkgIdxTs)
+    f (l,0) = (PkgIdxTs (fromIntegral l), Nothing)
+    f (l,b) = (PkgIdxTs (fromIntegral (min l b)), Just (PkgIdxTs (fromIntegral (max l b))))
+
+irToSet :: IR.IdxRanges -> Set.Set PkgIdxTs
+irToSet ir = Set.fromList [ PkgIdxTs j | j <- [ (-2) .. 260 ], IR.member (PkgIdxTs j) ir ]
+
 
 deriving instance Arbitrary PkgIdxTs
