225edcf25014251e0bfb85fc2fed5bc4a08f39f5
Herbert Valerio Riedel <hvr@gnu.org>
2020-03-29 13:20:00 +0200

    Update base-image to include ghc-8.8.3 & 8.10.1

--- worker-setup/build.sh
+++ worker-setup/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 
-mkdir -p bin
+mkdir -p worker/bin
 
 # cabal executable
-if [ ! -x bin/cabal ]; then
+if [ ! -x worker/bin/cabal ]; then
   echo "missing cabal executable"
   exit 1
 fi
 
 # worker
 cabal v2-build exe:matrix-worker
-cp -v "$(cabal new-exec which -- matrix-worker)" bin/
-strip -v bin/matrix-worker
+cp -v "$(cabal new-exec which -- matrix-worker)" ./worker/bin/
+strip -v worker/bin/matrix-worker
 
 # 
-docker build -t matrix-worker-base -f base.Dockerfile .
-docker build -t matrix-worker .
+docker build -t matrix-worker-base ./worker-base
+docker build -t matrix-worker      ./worker
 
